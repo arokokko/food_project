4bfd12fa5decc94c95dbcf249b444bed3059c1f2
arokokko <56476810+arokokko@users.noreply.github.com>
2020-10-14 11:15:49 +0700

    Realize module structure - create dir <modules> and file <tabs.js>

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,6 @@
+import tabs from './modules/tabs';
+
 window.addEventListener('DOMContentLoaded', () => {
-    const tabsCont = document.querySelectorAll('.tabcontent'),
-        tabsHeader = document.querySelector('.tabheader__items'),
-        tabsItems = document.querySelectorAll('.tabheader__item');
-
-    function hideContent(selectorContent, selectorTab, active) {
-        selectorContent.forEach((item) => {
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
-        });
-        selectorTab.forEach(tab => {
-            tab.classList.remove(active);
-        });
-    }
-    
-    function showOneTab(selectorContent, selectorTab, active, num = 0) {
-        selectorContent[num].classList.remove('hide');
-        selectorContent[num].classList.add('show', 'fade');
-        selectorTab[num].classList.add(active);
-    }
-
-    hideContent(tabsCont, tabsItems, 'tabheader__item_active');
-    showOneTab(tabsCont, tabsItems, 'tabheader__item_active');
-    switchTabs();
-
-    function switchTabs() {
-        tabsHeader.addEventListener('click', (e) => {
-            tabsItems.forEach((item, i) => {
-                if(e.target && (e.target == item || e.target.nodeParent == item)) {
-                    hideContent(tabsCont, tabsItems, 'tabheader__item_active');
-                    showOneTab(tabsCont, tabsItems, 'tabheader__item_active', i);
-                }
-            });
-        })
-    }
+    tabs();
     
 });
