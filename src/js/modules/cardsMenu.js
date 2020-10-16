61f845b7d44a6dd1938122272fc1be92d43949f3
arokokko <56476810+arokokko@users.noreply.github.com>
2020-10-16 20:29:19 +0700

    Refactoring menu cards, add default class 'menu__item'

--- src/js/modules/cardsMenu.js
+++ src/js/modules/cardsMenu.js
@@ -1,11 +1,12 @@
 const cardsMenu = () => {
     class Cards {
-        constructor(src, alt, title, desrc, price, parentSelector) {
+        constructor(src, alt, title, desrc, price, parentSelector, ...classes) {
             this.src = src,
             this.alt = alt, 
             this.title = title,
             this.descr = desrc,
             this.price = price,
+            this.classes = classes,
             this.parent = document.querySelector(parentSelector),
             this.rate = 27,
             this.getUAH()
@@ -15,8 +16,15 @@ const cardsMenu = () => {
         }
         render() {
             const div = document.createElement('div');
+            
+            if(this.classes.length === 0) {
+                this.classes = "menu__item";
+                div.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => div.classList.add(className));
+            }
+            
             div.innerHTML = `
-            <div class="menu__item">
                 <img src=${this.src} alt=${this.altlt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -25,7 +33,6 @@ const cardsMenu = () => {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
-            </div>
             `;
             this.parent.append(div);
         }
@@ -37,7 +44,8 @@ const cardsMenu = () => {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        ".menu .container"
+        ".menu .container",
+        "menu__item"
     ).render();
 
     const secondCard = new Cards(
@@ -46,7 +54,8 @@ const cardsMenu = () => {
         'Меню "Премиум"',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         14,
-        ".menu .container"
+        ".menu .container",
+        "menu__item"
     ).render();
 
     const thirdCard = new Cards(
@@ -55,7 +64,8 @@ const cardsMenu = () => {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         19,
-        ".menu .container"
+        ".menu .container",
+        "menu__item"
     ).render();
 }
 
